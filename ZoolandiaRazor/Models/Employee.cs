e6c2f59153bf6e3e772f444cb4ae0a15b353e3ef
MATT BRUTON <the.bruton@gmail.com>
2016-10-06 16:05:16 -0500

    added employee base employee model

---
+++ ZoolandiaRazor/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZoolandiaRazor.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Habitat> AssignedHabitats { get; set; }
+        public bool IsLicensedGorillaHandler { get; set; }
+    }
+}
