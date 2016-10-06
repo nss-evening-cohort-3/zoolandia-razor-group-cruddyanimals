6eb274ace75b55c6929e8396ea52ab32cc53d25d
Sylvia Ma <xiangwma@umich.edu>
2016-10-06 09:31:52 -0500

    habitat model

---
+++ ZoolandiaRazor/Models/Habitat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoolandiaRazor.Models
+{
+    public class Habitat
+    {
+        public int HabitatId { get; set; }
+        public string HabitatName { get; set; }
+        public string HabitatType { get; set; }
+        public List<Animal> AnimalList { get; set; }
+        public List<Employee> EmployeeList { get; set; }
+    }
+}
